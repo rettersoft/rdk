5c26ac3dbc3f659d936a831494cc51af97f05625
mdogukansahin <mustafadogukansahin@gmail.com>
2022-08-31 14:11:28 +0300

    limitErrorlar error olarak güncellendi.

--- src/index.ts
+++ src/index.ts
@@ -448,9 +448,9 @@ async function invokeLambda(payload: OperationsInput): Promise<OperationsOutput>
             concurrentLambdaCount--
             if (e) throw new Error(e)
             const r = JSON.parse(response as string)
-            if (r.limitError)
-                throw new Error(r.limitError)
-            delete r.limitError
+            if (r.error)
+                throw new Error(r.error)
+            delete r.error
             return r as OperationsOutput
         })
 
